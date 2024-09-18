369d96fd7fdff99e24ced7a6cb7fc9ba1786883c
StefanFroese1 <Froeses@stu.blsd.ca>
2024-09-18 12:01:31 -0500

    hi

--- Simulation/src/water_simulation/MyFrame.java
+++ Simulation/src/water_simulation/MyFrame.java
@@ -1,5 +1,19 @@
 package water_simulation;
 
-public class MyFrame {
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame{
+	MyPanel panel;
+	
+	MyFrame(){
+		panel = new MyPanel();
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		this.setUndecorated(true);
+		this.add(panel);
+		this.pack();
+		this.setVisible(true);
+	}
 
 }
--- Simulation/src/water_simulation/MyPanel.java
+++ Simulation/src/water_simulation/MyPanel.java
@@ -1,5 +1,28 @@
 package water_simulation;
 
-public class MyPanel {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+public class MyPanel extends JPanel implements ActionListener{
+	
+	private Timer timer;
+	
+	MyPanel(){
+		this.setBackground(Color.cyan);
+		timer = new Timer(50, this);
+		timer.start();
+		//test
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
+
