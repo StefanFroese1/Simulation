433975e83140fea0d06b51dfe062657731e6dd5b
Ben <bendrem@icloud.com>
2024-09-18 11:27:04 -0500

    added simple damage calculation (change later)

--- Simulation/src/water_simulation/Buildings.java
+++ Simulation/src/water_simulation/Buildings.java
@@ -1,5 +1,17 @@
 package water_simulation;
 
 public class Buildings {
+	
+	// give numbers in GUI pixels.
+	int height;
+	int elevation;
+	int damage;
+	
+	public int GetDamage(int tide,int wave){
+		
+		damage=(height+elevation)-tide;
+		
+		return damage;
+	}
 
 }
