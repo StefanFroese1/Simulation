c9bc729bfac0ea2ac853e65e8926ceb2be79c51c
Ben <bendrem@icloud.com>
2024-09-18 12:15:21 -0500

    water

--- Simulation/src/water_simulation/Water.java
+++ Simulation/src/water_simulation/Water.java
@@ -2,4 +2,16 @@ package water_simulation;
 
 public class Water {
 
+	int tide, wave;
+	public Water(int tideIn,int waveIn) {
+		tide=tideIn;
+		wave=waveIn;
+	}
+	public int GetWave() {
+		return wave;
+	}
+	public int GetTide() {
+		return tide;
+	}
+	
 }
