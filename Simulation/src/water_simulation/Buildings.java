2412497834a55ee45af955b3a4d48951b453f0e4
Ben <bendrem@icloud.com>
2024-09-18 11:36:16 -0500

    added simple demo for the damage calculator

--- Simulation/src/water_simulation/Buildings.java
+++ Simulation/src/water_simulation/Buildings.java
@@ -2,10 +2,15 @@ package water_simulation;
 
 public class Buildings {
 	
-	// give numbers in GUI pixels.
 	int height;
 	int elevation;
 	int damage;
+	// give numbers in GUI pixels.
+	public Buildings(int A, int B) {
+		height=A;
+		elevation=B;
+		
+	}
 	
 	public int GetDamage(int tide,int wave){
 		
--- Simulation/src/water_simulation/Main.java
+++ Simulation/src/water_simulation/Main.java
@@ -3,6 +3,8 @@ package water_simulation;
 public class Main {
 
 	public static void main(String[] args) {
+		Buildings A=new Buildings(30,40);
+		System.out.print(A.GetDamage(50,0));
 		
 	}
 
