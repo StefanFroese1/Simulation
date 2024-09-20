15cbc016eeb693c60748d43680ea23a8ff56d079
FroeseS <FroeseS@wcmp15vs014lt.blads.ca>
2024-09-20 11:14:01 -0500

    add

--- Simulation/src/water_simulation/MyPanel.java
+++ Simulation/src/water_simulation/MyPanel.java
@@ -3,6 +3,7 @@ package water_simulation;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -12,12 +13,16 @@ public class MyPanel extends JPanel implements ActionListener{
 	
 	private Timer timer;
 	private Water w = new Water(0, 0);
+	private Image slider;
+	private Image tab;
 	private int gridWidth = 160;
 	private int gridHight = 90;
 	private int pixelSize = 8;
 	
 	MyPanel(){
 		this.setBackground(Color.cyan);
+		tab = new ImageIcon("Assets/Tab.png").getImage();
+		slider = new ImageIcon("Assets/Slider.png").getImage();
 		timer = new Timer(50, this);
 		timer.start();
 		
@@ -26,6 +31,7 @@ public class MyPanel extends JPanel implements ActionListener{
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics2D g2D = (Graphics2D) g;
+		g2D.drawImage(slider, 0, 0, 100, 20, null);
 		for (int i=0;i<gridWidth;i++) {
 			for (int i2=0;i2<gridHight;i2++) {
 				switch(w.GetPixel(i,i2)) {
