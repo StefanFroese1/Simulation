a2b4dd3192834f19e652ccf8e1194b5d7a2b4862
Ben <bendrem@icloud.com>
2024-10-02 12:09:41 -0500

    final final changes

--- Simulation/src/water_simulation/Water.java
+++ Simulation/src/water_simulation/Water.java
@@ -72,7 +72,7 @@ public class Water {
 		
 		for (int i1=0;i1<159;i1++) {
 			for(int i2=88;i2>0;i2--) {
-				if(display[i1][i2+1]==0&&display[i][i2]==1) {
+				if(display[i1][i2+1]==0&&display[i1][i2]==1) {
 					display[i1][i2]=0;
 					display[i1][i2+1]=1;
 				}
